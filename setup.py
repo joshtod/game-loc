2a9715f0b42094449d8ff3279f8a502e551fbff4
joshtod <134558203+joshtod@users.noreply.github.com>
2023-10-29 13:54:11 -0500

    Initial upload, 10-29 snapshot

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An analysis of relative interest in games of specific types/genres within different language markets, intended to help games publishers prioritize their localization language selections.',
+    author='Joshua Ogden-Davis',
+    license='MIT',
+)
